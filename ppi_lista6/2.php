eb99b2b7d3c0f46b78aeb043a12a1f696fed911d
João Victor Alves <joao.v2007@aluno.ifsc.edu.br>
2025-06-24 20:01:39 -0300

    Lista 6

---
+++ ppi_lista6/2.php
@@ -0,0 +1,5 @@
+<?php
+    //O PHP é executado no navegador assim como o JavaScript? Explique.
+    
+    echo "Não, pois o PHP roda no servidor, e o JavaScript no navegador.";
+?>
---
+++ ppi_lista6/4.php
@@ -0,0 +1,8 @@
+<?php
+function randomize() {
+    echo rand(1, 100); // Número aleatório entre 1 e 100
+}
+
+// Chamando a função:
+randomize();
+?>
